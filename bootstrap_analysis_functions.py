6fec60656a508846c204c0ce61c72e624fe81f5c
guillermodeandajauregui@gmail.com <guillermodeandajauregui@gmail.com>
2016-07-29 18:14:37 -0500

    added function only file; added function for sorting subsetting graph by an MI interaction

---
+++ bootstrap_analysis_functions.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 29 11:30:36 2016
+
+@author: gdj
+"""
+
+def graphfromsif(sif):
+    s = pd.read_table(sif,
+                      header =None)
+    s = s[[0,2,1]]
+    s = s.values.tolist()
+    def formatfunction(lista):
+        return "{} {} {}".format(lista[0], lista[1], lista[2])
+    reformat = []
+    for elem in s:
+        reformat.append(formatfunction(elem))
+    g = nx.parse_edgelist(reformat, 
+                           nodetype = str, 
+                           data=(('weight',float),)
+                           )
+    return(g)
+#MI p-value relationship
+N = ""
+
+def pvalue(mi, n):
+    alfa = 1.062
+    beta = -48.7
+    gamma = -0.634
+    #MI = (alfa - logP) / (-beta + (-gamma * n))
+    p = math.exp(alfa -mi*(-beta + (-gamma * n)))
+    return(p)
+
+def threshold_subset(g, threshold):
+    
+    edges= [(from_node,to_node,edge_attributes) for from_node,to_node,edge_attributes in g.edges(data=True) if edge_attributes['weight'] > threshold]
+    gg = nx.Graph()
+    gg.add_edges_from(edges)
+    return(gg)
